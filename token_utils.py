147381ebdcd43df1fcb904f34cbc37ebd5f86fea
echickering <echickering@gmail.com>
2023-12-29 11:04:15 -0500

    handle token_cache file appropriately

--- token_utils.py
+++ token_utils.py
@@ -1,24 +1,24 @@
-# token_utils.py
-
 import requests
 import json
 import time
 import os
 from requests.exceptions import RequestException
 
-def obtain_api_token(client_id, client_secret, tsg_id, token_file, force_refresh=False): ##using token_file argument
+def obtain_api_token(client_id, client_secret, tsg_id, token_file, force_refresh=False):
     """
     Obtain and return the API token. If force_refresh is False and a valid token is cached, 
     return the cached token. Otherwise, request a new token and cache it.
     """
     if not force_refresh and os.path.exists(token_file):
-        with open(token_file, "r") as f:
-            token_data = json.load(f)
+        try:
+            with open(token_file, "r") as f:
+                token_data = json.load(f)
 
-        if time.time() < token_data["expires_at"]:
-            # print("Token is not expired, capturing and storing api token")
-            # print(token_data)
-            return token_data["access_token"]
+            if time.time() < token_data.get("expires_at", 0):
+                return token_data.get("access_token")
+        except json.JSONDecodeError:
+            # Handle empty file or invalid JSON
+            pass
 
     print("Fetching new Token because it's expired or force_refresh is set")
 
@@ -29,9 +29,8 @@ def obtain_api_token(client_id, client_secret, tsg_id, token_file, force_refresh
     response = requests.post(url, data=payload, headers=headers, auth=(client_id, client_secret))
 
     if response.status_code == 200:
-        data = json.loads(response.text)
+        data = response.json()
         token = data["access_token"]
-        # print(token)
         expires_in = data["expires_in"]
 
         token_data = {

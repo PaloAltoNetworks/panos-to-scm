b43ab70ba176ba8d9f083502dae3fee3d40408e6
echickering <echickering@gmail.com>
2024-01-23 14:00:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from parse.parse_panos import XMLParser
 from api import PanApiSession
 from scm import PanApiHandler
 from scm.process import Processor, SCMObjectManager
+from api.palo_token import PaloToken
+from panos import PaloConfigManager
 import scm.obj as obj
 
 def setup_logging():
@@ -25,6 +27,25 @@ def setup_logging():
     console_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
     logger.addHandler(console_handler)
 
+def get_xml_file_path(config, logger):
+    user_choice = input("Do you want to retrieve new config from Palo Alto NGFW? (yes/no): ").strip().lower()
+    xml_file_path = "running_config.xml"  # Default to the downloaded config
+
+    if user_choice == 'yes':
+        palo_token_manager = PaloToken()
+        token = palo_token_manager.retrieve_token()
+        palo_config_manager = PaloConfigManager(token, palo_token_manager.ngfw_url)
+        running_config = palo_config_manager.get_running_config()
+
+        with open(xml_file_path, "w") as file:
+            file.write(running_config)
+        logger.info("New running configuration retrieved and saved.")
+    else:
+        xml_file_path = config.xml_file_path
+        logger.info(f"Using local XML file: {xml_file_path}")
+
+    return xml_file_path
+
 def initialize_api_session():
     session = PanApiSession()
     session.authenticate()
@@ -37,7 +58,7 @@ def main(config):
     setup_logging()
     logger = logging.getLogger(__name__)
     begin_time = time.time()
-
+  
     try:
         start_time = time.time()
         print(f"Script started at {time.ctime(start_time)}")
@@ -45,7 +66,8 @@ def main(config):
         api_handler = PanApiHandler(initialize_api_session())
         configure = Processor(api_handler, config.max_workers, obj)
 
-        xml_file_path = config.xml_file_path
+        xml_file_path = get_xml_file_path(config, logger)
+
         parse = XMLParser(xml_file_path, None)
         folder_scope, config_type, device_group_name = parse.parse_config_and_set_scope(xml_file_path)
         logger.info(f'Current SCM Folder: {folder_scope}, PANOS: {config_type}, Device Group: {device_group_name}')

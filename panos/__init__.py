815e32563054f6663c3f154df50bca00828c571d
echickering <echickering@gmail.com>
2024-01-23 12:49:37 -0500

    SSLError Bypass prompt
    added ability to bypass SSLError

--- panos/__init__.py
+++ panos/__init__.py
@@ -1,23 +1,34 @@
 import requests
-import urllib3
 import xml.etree.ElementTree as ET
+from requests.exceptions import SSLError
 
 class PaloConfigManager:
     def __init__(self, token, base_url):
-        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
         self.token = token
         self.base_url = base_url
 
     def get_running_config(self):
         headers = {'X-PAN-KEY': self.token}
         payload = {'type': 'config', 'action': 'show', 'xpath': '/config/'}
-        response = requests.get(self.base_url, headers=headers, params=payload, verify=False)
+        try:
+            response = self.make_request(headers, payload)
+        except SSLError as e:
+            user_input = input(f"Untrusted certificate from {self.base_url} - continue? (yes/no): ").strip().lower()
+            if user_input == 'yes':
+                response = self.make_request(headers, payload, verify=False)
+            else:
+                raise e
+
         if response.status_code == 200:
             root = ET.fromstring(response.content)
             config_element = root.find('.//config')
             if config_element is not None:
+                print(f'Successfully downloaded XML configuration from: {self.base_url}')
                 return ET.tostring(config_element, encoding='unicode')
             else:
                 raise Exception("No config element found in the response")
         else:
             raise Exception("Failed to retrieve running configuration")
+
+    def make_request(self, headers, payload, verify=True):
+        return requests.get(self.base_url, headers=headers, params=payload, verify=verify)
